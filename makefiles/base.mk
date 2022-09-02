706222a6e8547e5f664e8559e92db9114ec1df4e
jesmigel <jesmigel.developer@gmail.com>
2022-09-02 13:21:38 +1000

    AT3-264 fixed makefile paths

--- makefiles/base.mk
+++ makefiles/base.mk
@@ -1,7 +1,7 @@
 # Common make targets for pst build products
 
 # Containerised local development environment
-include .make/devenv.mk
+include devenv.mk
 
 # CPP development make targets
-include .make/cpp.mk
+include cpp.mk
