8712bb551c083b84d53524ac21132bf421aae11d
jesmigel <jesmigel.developer@gmail.com>
2022-09-05 12:01:49 +1000

    AT3-264 updated dev env vars

--- makefiles/devenv.mk
+++ makefiles/devenv.mk
@@ -1,10 +1,11 @@
 .PHONY: local-docker-build local-docker-run
-# OCI_IMAGE=? # Declare in PrivateRules.mak for invoking local development environment local-dev-env
-# OCI_TAG=`grep -m 1 -o "[0-9].*" .release` # Declare in PrivateRules.mak for invoking local development environment local-dev-env
+# DEV_IMAGE=? # Declare in PrivateRules.mak for invoking local development environment local-dev-env
+# DEV_TAG=`grep -m 1 -o "[0-9].*" .release` # Declare in PrivateRules.mak for invoking local development environment local-dev-env
+
 local-dev-env:
 	@echo 'OCI_IMAGE: $(OCI_IMAGE)'
 	@echo 'OCI_TAG: $(OCI_TAG)'
-	@$(OCI_BUILDER) run --rm -ti -v $(PWD):/mnt/$(OCI_IMAGE) -w /mnt/$(OCI_IMAGE) $(OCI_IMAGE):$(OCI_TAG) bash
+	@$(OCI_BUILDER) run --rm -ti -v $(PWD):/mnt/$(OCI_IMAGE) -w /mnt/$(OCI_IMAGE) $(DEV_IMAGE):$(DEV_TAG) bash
 
 local-docker-run:
 	docker run -it --rm -v $(PWD):/mnt/$(PROJECT) -t $(strip $(OCI_IMAGE)):$(VERSION) $(RUN_ARGS)
