c0e49710a43bdfa910f4bce342fda78b3617f3be
jesmigel <jesmigel.developer@gmail.com>
2022-09-07 10:40:52 +1000

    AT3-264 update common makefiles location - added .pst submodule to contain common makefile targets.

--- makefiles/base.mk
+++
@@ -1,8 +0,0 @@
-# Common make targets for pst build products
-
-PST_MAKE_PATHS=.pst/makefiles
-# Containerised local development environment
-include $(PST_MAKE_PATHS)/devenv.mk
-
-# CPP development make targets
-include $(PST_MAKE_PATHS)/cpp.mk
