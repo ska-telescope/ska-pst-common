c5b9c4f3b5320d7c6a6028c2ea551938146d36a5
Will Gauvin <wgauvin@swin.edu.au>
2023-05-02 10:15:20 +1000

    AT3-348 bump patch release to 0.8.5

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -51,7 +51,7 @@ author = 'PST Team'
 # The full version, including alpha/beta/rc tags
 with open('../../.release') as f:
     version = f.readline().strip().split("=")[1]
-release = '0.8.4'
+release = '0.8.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
