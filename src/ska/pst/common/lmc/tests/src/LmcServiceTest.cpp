34dc25d263adc45abace4ec93eaae41729e4ba23
jesmigel <jesmigel.developer@gmail.com>
2023-05-18 16:50:57 +1000

    AT3-407 removed redeclaration of status

--- src/ska/pst/common/lmc/tests/src/LmcServiceTest.cpp
+++ src/ska/pst/common/lmc/tests/src/LmcServiceTest.cpp
@@ -1728,25 +1728,25 @@ TEST_F(LmcServiceTest, set_and_get_log_levels) // NOLINT
     request.set_log_level(ska::pst::lmc::LogLevel::INFO);
     set_log_level(request);
     ASSERT_EQ(spdlog::level::info, spdlog::get_level());
-    auto status = get_log_level(response);
+    status = get_log_level(response);
     EXPECT_TRUE(status.ok());
 
     request.set_log_level(ska::pst::lmc::LogLevel::WARNING);
     set_log_level(request);
     ASSERT_EQ(spdlog::level::warn, spdlog::get_level());
-    auto status = get_log_level(response);
+    status = get_log_level(response);
     EXPECT_TRUE(status.ok());
 
     request.set_log_level(ska::pst::lmc::LogLevel::CRITICAL);
     set_log_level(request);
     ASSERT_EQ(spdlog::level::critical, spdlog::get_level());
-    auto status = get_log_level(response);
+    status = get_log_level(response);
     EXPECT_TRUE(status.ok());
     
     request.set_log_level(ska::pst::lmc::LogLevel::ERROR);
     set_log_level(request);
     ASSERT_EQ(spdlog::level::err, spdlog::get_level());
-    auto status = get_log_level(response);
+    status = get_log_level(response);
     EXPECT_TRUE(status.ok());
 }
 
