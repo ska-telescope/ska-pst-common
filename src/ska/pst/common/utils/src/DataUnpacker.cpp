6cba079b8b7b8a35c6282b0910e1e871ad5ebc80
Willem van Straten <vanstraten.willem@gmail.com>
2023-08-02 16:49:24 +1200

    AT3-517 memory layout of packet heaps abstracted into HeapLayout class

--- src/ska/pst/common/utils/src/DataUnpacker.cpp
+++ src/ska/pst/common/utils/src/DataUnpacker.cpp
@@ -36,54 +36,13 @@
 
 void ska::pst::common::DataUnpacker::configure(const ska::pst::common::AsciiHeader& data_config, const ska::pst::common::AsciiHeader& weights_config)
 {
+  layout.configure(data_config, weights_config);
+
   // extract the required parameters from the data header
   ndim = data_config.get_uint32("NDIM");
   npol = data_config.get_uint32("NPOL");
   nbit = data_config.get_uint32("NBIT");
   nchan = data_config.get_uint32("NCHAN");
-  nsamp_per_packet = data_config.get_uint32("UDP_NSAMP");
-  nchan_per_packet = data_config.get_uint32("UDP_NCHAN");
-  nsamp_per_weight = data_config.get_uint32("WT_NSAMP");
-
-  // extract parameters from the weights header
-  weights_nbit = weights_config.get_uint32("NBIT");
-  weights_packet_stride = weights_config.get_uint32("PACKET_WEIGHTS_SIZE") + weights_config.get_uint32("PACKET_SCALES_SIZE");
-  SPDLOG_DEBUG("ska::pst::common::DataUnpacker::configure weights nbit={} packet_stride={}", weights_nbit, weights_packet_stride);
-
-  if (ndim != 2)
-  {
-    SPDLOG_ERROR("ska::pst::common::DataUnpacker::configure expected NDIM=2, but found {}", ndim);
-    throw std::runtime_error("ska::pst::common::DataUnpacker::configure invalid NDIM");
-  }
-
-  if (npol != 2)
-  {
-    SPDLOG_ERROR("ska::pst::common::DataUnpacker::configure expected NPOL=2, but found {}", npol);
-    throw std::runtime_error("ska::pst::common::DataUnpacker::configure invalid NPOL");
-  }
-
-  if (nbit != 8 && nbit != 16) // NOLINT
-  {
-    SPDLOG_ERROR("ska::pst::common::DataUnpacker::configure expected NBIT=8 or 16, but found {}", nbit);
-    throw std::runtime_error("ska::pst::common::DataUnpacker::configure invalid NBIT");
-  }
-
-  SPDLOG_DEBUG("ska::pst::common::DataUnpacker::configure nchan={} layout.get_nchan_per_packet()={}", nchan, nchan_per_packet);
-  if (nchan % nchan_per_packet != 0)
-  {
-    SPDLOG_ERROR("ska::pst::common::DataUnpacker::configure NCHAN={} was not a multiple of nchan_per_packet={}", nchan, nchan_per_packet);
-    throw std::runtime_error("ska::pst::common::SineWaveGenerator::configure invalid NCHAN");
-  }
-
-  nsamp_per_weight = nsamp_per_packet; // compliant with all known ICD formats, but not robust to new formats
-  packet_resolution = nsamp_per_packet * nchan_per_packet * npol * ndim * nbit / ska::pst::common::bits_per_byte;
-  heap_resolution = nsamp_per_packet * nchan * npol * ndim * nbit / ska::pst::common::bits_per_byte;
-  if (heap_resolution % packet_resolution)
-  {
-    SPDLOG_WARN("ska::pst::common::DataUnpacker::configure heap_resolution[{}] was not a multiple of packet resolution[{}]", heap_resolution, packet_resolution);
-    throw std::runtime_error("ska::pst::common::DataUnpacker::configure invalid heap/packet resolution");
-  }
-  packets_per_heap = heap_resolution / packet_resolution;
 
   bandpass.resize(nchan);
   for (unsigned ichan=0; ichan<nchan; ichan++)
@@ -95,7 +54,7 @@ void ska::pst::common::DataUnpacker::configure(const ska::pst::common::AsciiHead
 
 auto ska::pst::common::DataUnpacker::get_scale_factor(char * weights, uint32_t packet_number) -> float
 {
-  auto * weights_ptr = reinterpret_cast<float *>(weights + (packet_number * weights_packet_stride)); // NOLINT
+  auto * weights_ptr = reinterpret_cast<float *>(weights + (packet_number * layout.get_weights_packet_stride())); // NOLINT
   // return the scale factor, ignoring invalid value of 0
   if (*weights_ptr == 0) {
     return 1;
@@ -149,11 +108,11 @@ auto ska::pst::common::DataUnpacker::unpack(char * data, uint64_t data_bufsz, ch
 
   resize(data_bufsz);
 
-  const uint32_t nheaps = data_bufsz / heap_resolution;
-  SPDLOG_DEBUG("ska::pst::common::DataUnpacker::unpack heap_resolution={} nheaps={}", heap_resolution, nheaps);
+  const uint32_t nheaps = data_bufsz / layout.get_data_heap_stride();
+  SPDLOG_DEBUG("ska::pst::common::DataUnpacker::unpack data_heap_stride={} nheaps={}", layout.get_data_heap_stride(), nheaps);
 
   SPDLOG_DEBUG("ska::pst::common::DataUnpacker::unpack packets_per_heap={} npol={} nchan_per_packet={} nsamp_per_packet={}",
-    packets_per_heap, npol, nchan_per_packet, nsamp_per_packet);
+    layout.get_packets_per_heap(), npol, layout.get_packet_layout().get_nchan_per_packet(), layout.get_packet_layout().get_samples_per_packet());
 
   // unpack the 8 or 16 bit signed integers
   if (nbit == 8) // NOLINT
@@ -180,10 +139,10 @@ void ska::pst::common::DataUnpacker::integrate_bandpass(char * data, uint64_t da
     reinterpret_cast<void*>(data), data_bufsz, reinterpret_cast<void *>(weights), weights_bufsz);
 
   // unpack the heaped CBF/PSR format into TFP ordered timeseries
-  const uint32_t nheaps = data_bufsz / heap_resolution;
+  const uint32_t nheaps = data_bufsz / layout.get_data_heap_stride();
 
   SPDLOG_DEBUG("ska::pst::common::DataUnpacker::integrate_bandpass nheaps={} packets_per_heap={} npol={} nchan_per_packet={} nsamp_per_packet={}",
-    nheaps, packets_per_heap, npol, nchan_per_packet, nsamp_per_packet);
+    nheaps, layout.get_packets_per_heap(), npol, layout.get_packet_layout().get_nchan_per_packet(), layout.get_packet_layout().get_samples_per_packet());
 
   // integerate the 8 or 16 bit signed integers
   if (nbit == 8) // NOLINT
